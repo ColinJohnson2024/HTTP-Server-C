21fdde2f9e1997b00c6fcf532f72960ca68d220b
Colin Johnson <colinjohnson@Colin-Laptop.localdomain>
2025-07-07 00:02:20 -0500

    First commit. Simple main.c written. Early dev stage.

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+int main() {
+    printf("Creating socket...\n");
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    printf("Socket created successfully! File descriptor: %d\n", server_fd);
+
+    close(server_fd);
+    return 0;
+}
